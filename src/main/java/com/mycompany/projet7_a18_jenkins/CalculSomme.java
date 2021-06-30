489d89444d46670cc7459722cb575dbb4fd165ae
imed <imadferfar@yahoo.com>
2021-06-30 15:26:28 -0400

    commit n 2 Class CalculSomme:

---
+++ src/main/java/com/mycompany/projet7_a18_jenkins/CalculSomme.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projet7_a18_jenkins;
+
+/**
+ *
+ * @author BaDRi
+ */
+    
+    public class CalculSomme {
+    public static int CalculSomme(int valeur1, int valeur2){
+        return valeur1 + valeur2;
+        
+    }
+    
+}
+
+  
